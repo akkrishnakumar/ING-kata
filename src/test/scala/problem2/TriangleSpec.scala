ab82f4274e2a6c485bf2f432c16e10fbe3b24576
Akhil Krishnakumar <akhil.krishnakumar.iicmr@gmail.com>
2021-07-19 16:50:39 +0530

    Create failing test - Area of triangle with base 10 and height 10 should be 50

---
+++ src/test/scala/problem2/TriangleSpec.scala
@@ -0,0 +1,14 @@
+package problem2
+
+import utils.BaseSpec
+
+class TriangleFormulaeSpec extends BaseSpec {
+
+  test("Area of triangle with base 10 and height 10 should be 50") {
+    val expectedArea: Double = 50.0
+
+    val actualArea: Double = 0
+
+    actualArea shouldEqual expectedArea
+  }
+}
