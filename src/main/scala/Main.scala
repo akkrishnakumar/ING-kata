2d2ba8babdd7b5af679edae4834a6c495970b2e5
Akhil Krishnakumar <akhil.krishnakumar.iicmr@gmail.com>
2021-07-19 21:29:28 +0530

    Add Problem 3 to Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
 import problem1.ConsolePrinter
 import problem2.Triangle
+import problem3.FindSmallest
+import problem3.FootballData
+import problem3.Ripper
 object Main extends App {
 
   println("=" * 100)
@@ -19,6 +22,19 @@ object Main extends App {
   val triangle = new Triangle(10, 10)
 
   println(s"Area of Triangle with base 10 and height 10 is ${triangle.area}")
+
+  println("=" * 100)
+
+  println("Problem 3")
+
+  def smallestDifference(data: FootballData) = Math.abs(data.againstGoals - data.forGoals)
+
+  val team = FindSmallest[FootballData](smallestDifference)(Ripper[FootballData]("football.dat"))
+
+  println(
+    s"Team with the smallest difference in 'for' and 'against' goals is ${team.name} with" +
+      s" Goals For: ${team.forGoals} and Goals Against: ${team.againstGoals}"
+  )
 
   println("=" * 100)
 
