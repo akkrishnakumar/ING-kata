e8b4c77b1f52db4b8fade02fedb040edc3b5aed7
Akhil Krishnakumar <akhil.krishnakumar.iicmr@gmail.com>
2021-07-19 16:02:59 +0530

    Create Failing Test - Print n numbers to the console

---
+++ src/test/scala/problem1/ConsolePrintSpec.scala
@@ -0,0 +1,15 @@
+package problem1
+
+import utils.BaseSpec
+
+class ConsolePrintSpec extends BaseSpec {
+
+  test("Print n numbers to the console") {
+    val expected = "1,2,3,4,5,6,7,8,9,10"
+
+    val actual: String = ""
+
+    actual shouldEqual expected
+  }
+
+}
